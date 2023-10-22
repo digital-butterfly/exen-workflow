de2e065b25aa4ead8a16f8c3bf04a28857ef6de1
MeReda <medredaben14@gmail.com>
2023-10-22 12:11:40 +0100

    deleted browserChecker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import AuthProvider from './context/AuthProvider'
-import BrowserChecker from '@/utils/browserChecker'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <BrowserChecker />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
